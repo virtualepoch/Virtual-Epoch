bee27bf096c970f9faf92d9e906ad7ab80f9afb1
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-03-01 16:37:10 -0700

    3-1-22 4pm update
    Adjusted bottom link buttons

--- test.js
+++ test.js
@@ -24,37 +24,49 @@ var h4 = document.getElementById("h4");
 var h5 = document.getElementById("h5");
 
 window.onload = function indexHtagAnim() {
-  if (h2.className == "") {
     h2.className = "load";
     h3.className = "load";
     h4.className = "load";
     h5.className = "load";
-  }
+    actionbtn.style.display = "block";
+    actionbtn2.style.display = "none";
+    actionbtn3.style.display = "none";
+    actionbtn4.style.display = "none";
 };
 
 var home = document.getElementById("home");
 var about = document.getElementById("about");
 var projects = document.getElementById("projects");
 var contact = document.getElementById("contact");
-
+var actionbtn = document.getElementById("actionbtn");
+var actionbtn2 = document.getElementById("actionbtn2");
+var actionbtn3 = document.getElementById("actionbtn3");
+var actionbtn4 = document.getElementById("actionbtn4");
 
 function changeHome() {
   home.style.display = "block";
   about.style.display = "none";
   projects.style.display = "none";
   contact.style.display = "none";
+  actionbtn.style.display = "block";
+  actionbtn2.style.display = "none";
+  actionbtn3.style.display = "none";
+  actionbtn4.style.display = "none";
   menubox.className = "";
   link1.className = "";
   link2.className = "";
   link3.className = "";
   link4.className = "";
 }
-
 function changeAbout() {
   home.style.display = "none";
   about.style.display = "block";
   projects.style.display = "none";
   contact.style.display = "none";
+  actionbtn.style.display = "none";
+  actionbtn2.style.display = "block";
+  actionbtn3.style.display = "none";
+  actionbtn4.style.display = "none";
   menubox.className = "";
   link1.className = "";
   link2.className = "";
@@ -67,6 +79,10 @@ function changeProjects() {
   about.style.display = "none";
   projects.style.display = "block";
   contact.style.display = "none";
+  actionbtn.style.display = "none";
+  actionbtn2.style.display = "none";
+  actionbtn3.style.display = "block";
+  actionbtn4.style.display = "none";
   menubox.className = "";
   link1.className = "";
   link2.className = "";
@@ -78,6 +94,10 @@ function changeContact() {
   about.style.display = "none";
   projects.style.display = "none";
   contact.style.display = "block";
+  actionbtn.style.display = "none";
+  actionbtn2.style.display = "none";
+  actionbtn3.style.display = "none";
+  actionbtn4.style.display = "block";
   menubox.className = "";
   link1.className = "";
   link2.className = "";
