49d6bbc9d5e8b1af21abdb2772d192eb3416c1d8
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-04-01 17:30:03 -0600

    Minor tweaks

--- canvas1.js
+++ canvas1.js
@@ -34,7 +34,7 @@ class Effect {
   constructor(canvasWidth, canvasHeight) {
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
-    this.fontSize = 20;
+    this.fontSize = 15;
     this.columns = this.canvasWidth / this.fontSize;
     this.symbols = [];
     this.#initialize();
