9a8daf3cccd9b63da974955f7e949e94418da409
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-04-07 14:43:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var menuBox = document.getElementById("menu-box");
+const menuBox = document.getElementById("menu-box");
 
 function openMenu() {
   if (menuBox.className == "open") {
@@ -14,32 +14,32 @@ function openMenu() {
     link3.className = "open";
     link4.className = "open";
   }
-};
+}
 
-var h2 = document.getElementById("h2");
-var h3 = document.getElementById("h3");
-var h4 = document.getElementById("h4");
-var h5 = document.getElementById("h5");
+const h2 = document.getElementById("h2");
+const h3 = document.getElementById("h3");
+const h4 = document.getElementById("h4");
+const h5 = document.getElementById("h5");
 
 window.onload = function indexHtagAnim() {
-    h2.className = "load";
-    h3.className = "load";
-    h4.className = "load";
-    h5.className = "load";
-    actionbtn.style.display = "block";
-    actionbtn2.style.display = "none";
-    actionbtn3.style.display = "none";
-    actionbtn4.style.display = "none";
+  h2.className = "load";
+  h3.className = "load";
+  h4.className = "load";
+  h5.className = "load";
+  actionbtn.style.display = "block";
+  actionbtn2.style.display = "none";
+  actionbtn3.style.display = "none";
+  actionbtn4.style.display = "none";
 };
 
-var home = document.getElementById("home");
-var about = document.getElementById("about");
-var projects = document.getElementById("projects");
-var contact = document.getElementById("contact");
-var actionbtn = document.getElementById("actionbtn-page-1");
-var actionbtn2 = document.getElementById("actionbtn-page-2");
-var actionbtn3 = document.getElementById("actionbtn-page-3");
-var actionbtn4 = document.getElementById("actionbtn-page-4");
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
+const actionbtn = document.getElementById("actionbtn-page-1");
+const actionbtn2 = document.getElementById("actionbtn-page-2");
+const actionbtn3 = document.getElementById("actionbtn-page-3");
+const actionbtn4 = document.getElementById("actionbtn-page-4");
 
 function changeHome() {
   home.style.display = "block";
