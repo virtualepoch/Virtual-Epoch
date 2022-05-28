30f7e5b194482ea66c26b20abe6abcf5a629a16c
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-05-28 12:18:20 -0600

    onload function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
+const actionbtn = document.getElementById("actionbtn-page-1");
+const actionbtn2 = document.getElementById("actionbtn-page-2");
+const actionbtn3 = document.getElementById("actionbtn-page-3");
+const actionbtn4 = document.getElementById("actionbtn-page-4");
+const h2 = document.getElementById("h2");
+const h3 = document.getElementById("h3");
+const h4 = document.getElementById("h4");
+const h5 = document.getElementById("h5");
+
+window.onload = function loadHome() {
+  home.style.display = "block";
+  about.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+  actionbtn.style.display = "block";
+  actionbtn2.style.display = "none";
+  actionbtn3.style.display = "none";
+  actionbtn4.style.display = "none";
+  h2.className = "load";
+  h3.className = "load";
+  h4.className = "load";
+  h5.className = "load";
+  actionbtn.style.display = "block";
+  actionbtn2.style.display = "none";
+  actionbtn3.style.display = "none";
+  actionbtn4.style.display = "none";
+};
+
 const menuBtn = document.querySelector(".menu-btn");
+
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
@@ -28,31 +61,6 @@ function openMenu() {
   }
 }
 
-const h2 = document.getElementById("h2");
-const h3 = document.getElementById("h3");
-const h4 = document.getElementById("h4");
-const h5 = document.getElementById("h5");
-
-window.onload = function indexHtagAnim() {
-  h2.className = "load";
-  h3.className = "load";
-  h4.className = "load";
-  h5.className = "load";
-  actionbtn.style.display = "block";
-  actionbtn2.style.display = "none";
-  actionbtn3.style.display = "none";
-  actionbtn4.style.display = "none";
-};
-
-const home = document.getElementById("home");
-const about = document.getElementById("about");
-const projects = document.getElementById("projects");
-const contact = document.getElementById("contact");
-const actionbtn = document.getElementById("actionbtn-page-1");
-const actionbtn2 = document.getElementById("actionbtn-page-2");
-const actionbtn3 = document.getElementById("actionbtn-page-3");
-const actionbtn4 = document.getElementById("actionbtn-page-4");
-
 function changeHome() {
   home.style.display = "block";
   about.style.display = "none";
@@ -64,6 +72,7 @@ function changeHome() {
   actionbtn4.style.display = "none";
   menuBox.style.top = "";
 }
+
 function changeAbout() {
   home.style.display = "none";
   about.style.display = "block";
