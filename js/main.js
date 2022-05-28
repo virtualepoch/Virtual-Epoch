a7d49b145b0ebad929fba46654fe572e34c75d60
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-05-28 10:34:48 -0600

    menu-btn animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
+
 const menuBox = document.getElementById("menu-box");
 
 function openMenu() {
