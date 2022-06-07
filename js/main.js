8053e44d9f64453553ec798f79688b17a91c332c
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-06-07 16:49:19 -0600

    fixes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,9 @@
-const home = document.getElementById("home");
-const about = document.getElementById("about");
-const projects = document.getElementById("projects");
-const contact = document.getElementById("contact");
-const actionBtn = document.getElementById("action-btn-page-1");
-const actionBtn2 = document.getElementById("action-btn-page-2");
-const actionBtn3 = document.getElementById("action-btn-page-3");
-const actionBtn4 = document.getElementById("action-btn-page-4");
 const h2 = document.getElementById("h2");
 const h3 = document.getElementById("h3");
 const h4 = document.getElementById("h4");
 const h5 = document.getElementById("h5");
 
 window.onload = function loadHome() {
-  home.style.display = "block";
-  about.style.display = "none";
-  projects.style.display = "none";
-  contact.style.display = "none";
-  actionBtn.style.display = "block";
-  actionBtn2.style.display = "none";
-  actionBtn3.style.display = "none";
-  actionBtn4.style.display = "none";
   h2.className = "load";
   h3.className = "load";
   h4.className = "load";
@@ -62,6 +46,15 @@ menuBtn.addEventListener("mousedown", function () {
 menuBtn.addEventListener("mouseup", function () {
   menuBtn.style.boxShadow = "";
 });
+
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
+const actionBtn = document.getElementById("action-btn-page-1");
+const actionBtn2 = document.getElementById("action-btn-page-2");
+const actionBtn3 = document.getElementById("action-btn-page-3");
+const actionBtn4 = document.getElementById("action-btn-page-4");
 
 function changeHome() {
   home.style.display = "block";
